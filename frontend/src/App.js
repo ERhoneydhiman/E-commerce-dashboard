6e7ac9c29d2fb661c776942e633c99518e262b38
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-05-20 12:03:27 +0530

    ad comps and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import { RouterProvider , createBrowserRouter, Route, createRoutesFromElements } from 'react-router-dom';
 
+import Layout from './comps/Layout';
+import Products from './comps/Products';
+import AddProducts from './comps/AddProducts';
+import Profile from './comps/Profile';
+import Logout from './comps/Logout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+       <Route path='' element={<Products/>}/>
+       <Route path='/addproduct' element={<AddProducts/>}/>
+       <Route path='/updateproduct' element={<AddProducts/>}/>
+       <Route path='/profile' element={<Profile/>}/>
+       <Route path='/logout' element={<Logout/>}/>
+    </Route>
+  )
+)
 function App() {
   return (
     <div className="App">
-      <h1>hd!!!</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ frontend/src/comps/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function Nav() {
+  return (
+    <div className='header'>
+      <h1>E-Deshboard</h1>
+      <div className="nav">
+        <ul>
+            <li><Link to="/">Products</Link></li>
+            <li><Link to="/addproduct">Add Products</Link></li>
+            <li><Link to="/updateproduct">Update Products</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/logout">LogOut</Link></li>
+        </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
