a7ad88df0180bb9905878440cb898c888bb8c0bc
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-05-20 16:52:28 +0530

    add sign up api in signup page

--- frontend/src/comps/SignUp.js
+++ frontend/src/comps/SignUp.js
@@ -1,19 +1,50 @@
 import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom'
 
 function SignUp() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+    const [password, setPassword] = useState('');
+    const [signupmsg, setSignupmsg] = useState()
+    const navigate = useNavigate();
 
-    const collectData = ()=>{
-        console.log(name, pass, email)
-    }
+    const collectData = async () => {
+        const user = { name, email, password };
+        console.log(name, password, email);
+
+        try {
+            const response = await fetch('http://localhost:5000/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(user)
+            });
+
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.statusText}`);
+            }
+
+            const result = await response.json();
+            console.log(result);
+            setSignupmsg('User Sign Up Done')
+
+            if(response){
+                navigate('/')
+            }
+
+        } catch (error) {
+            console.error('Failed to fetch:', error);
+        }
+    };
+
 
     return (
         <div className='signup'>
             <h1>Register Here</h1>
             <div className='form'>
                 <input
+                    required
                     value={name}
                     className='input-box'
                     id='name'
@@ -23,6 +54,7 @@ function SignUp() {
                 />
 
                 <input
+                    required
                     value={email}
                     className='input-box'
                     id='email'
@@ -32,15 +64,17 @@ function SignUp() {
                 />
 
                 <input
-                    value={pass}
+                    required
+                    value={password}
                     className='input-box'
                     id='pass'
                     type="password"
                     placeholder='New Password'
-                    onChange={(e) => setPass(e.target.value)}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
 
                 <button onClick={collectData} id='signup-btn'>Sign Up</button>
+                <p>{signupmsg}</p>
             </div>
         </div>
     );
