b17958f4f7ab0a327a30ad2ff29192bafe792cd5
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-05-22 10:38:45 +0530

    login API

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,25 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.post('/signup',async (req, res)=>{
+app.post('/signup', async (req, res) => {
     let user = new User(req.body);
-    let result =await user.save();
+    let result = await user.save();
+    result = result.toObject()
+    delete result.password
     res.send(result)
+})
+
+app.post('/login', async (req, res) => {
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+            res.send(user)
+        } else {
+            res.send("no user found")
+        }
+    } else {
+        res.send("no user found")
+    }
 })
 
 app.listen(5000)
