ca7103ba1600aa88f3a248a437f8cd9f5582f430
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-05-29 15:33:33 +0530

    add jwt token

--- frontend/src/comps/Login.js
+++ frontend/src/comps/Login.js
@@ -1,86 +1,86 @@
-import React, { useState, useEffect } from 'react'
-import { useNavigate , Link } from 'react-router-dom';
-
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [signupmsg, setSignupmsg] = useState()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signupmsg, setSignupmsg] = useState();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const auth = localStorage.getItem('user');
-        if (auth) {
-            navigate('/')
-        }
-    })
-
-
-    const handleLogin = async () => {
-
-        if (email === "" || password === "") {
-            setSignupmsg("fill all input boxes")
-        } else {
-            console.log(email, password)
-            let result = await fetch('http://localhost:5000/login', {
-                method: 'post',
-                body: JSON.stringify({ email, password }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-            result = await result.json();
-            console.log(result)
-
-            if (result.name) {
-                console.log('Login successful');
-                localStorage.setItem("user", JSON.stringify(result))
-                navigate('/')
-            } else {
-                console.log('Login failed, showing alert');
-                alert('Enter correct details');
-                setEmail('')
-                setPassword('')
-            }
-        }
-
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
     }
+  });
 
-    return (
-        <div className='login'>
-            <h1>E-Comm-Deshboard</h1>
-            <h1>Login to access website</h1>
-            <div className="form">
-                <input
-                    required
-                    value={email}
-                    className='input-box'
-                    id='email'
-                    type="email"
-                    placeholder='Enter Email'
-                    onChange={(e) => setEmail(e.target.value)}
-                />
+  const handleLogin = async () => {
+    if (email === "" || password === "") {
+      setSignupmsg("fill all input boxes");
+    } else {
+      console.log(email, password);
+      let result = await fetch("http://localhost:5000/login", {
+        method: "post",
+        body: JSON.stringify({ email, password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      result = await result.json();
+      console.log(result);
 
-                <input
-                    required
-                    value={password}
-                    className='input-box'
-                    id='pass'
-                    type="password"
-                    placeholder='New Password'
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <p>{signupmsg}</p>
-                <button onClick={handleLogin} id='signup-btn'>Login</button>
+      if (result.auth) {
+        console.log("Login successful");
+        localStorage.setItem("user", JSON.stringify(result.user));
+        localStorage.setItem("token", JSON.stringify(result.auth));
+        navigate("/");
+      } else {
+        console.log("Login failed, showing alert");
+        alert("Enter correct details");
+        setEmail("");
+        setPassword("");
+      }
+    }
+  };
 
-            </div>
-            <div id="login">
-                <p>New User ?? then</p>
-                <button id='login-btn' ><Link to="/signup">Sign Up</Link></button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="login">
+      <h1>E-Comm-Deshboard</h1>
+      <h1>Login to access website</h1>
+      <div className="form">
+        <input
+          required
+          value={email}
+          className="input-box"
+          id="email"
+          type="email"
+          placeholder="Enter Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          required
+          value={password}
+          className="input-box"
+          id="pass"
+          type="password"
+          placeholder="New Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <p>{signupmsg}</p>
+        <button onClick={handleLogin} id="signup-btn">
+          Login
+        </button>
+      </div>
+      <div id="login">
+        <p>New User ?? then</p>
+        <button id="login-btn">
+          <Link to="/signup">Sign Up</Link>
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- frontend/src/comps/SignUp.js
+++ frontend/src/comps/SignUp.js
@@ -1,97 +1,99 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 function SignUp() {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [signupmsg, setSignupmsg] = useState()
-    const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signupmsg, setSignupmsg] = useState();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const auth = localStorage.getItem('user');
-        if (auth) {
-            navigate('/')
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    if (auth) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  const collectData = async () => {
+    if (name === "" || email === "" || password === "") {
+      setSignupmsg("fill all inputs");
+    } else {
+      const user = { name, email, password };
+      console.log(name, password, email);
+
+      try {
+        const response = await fetch("http://localhost:5000/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        });
+
+        const result = await response.json();
+        console.log(result);
+        setSignupmsg("User Sign Up Done");
+
+        if (response) {
+          navigate("/");
+          localStorage.setItem("user", JSON.stringify(result.result));
+          localStorage.setItem("token", JSON.stringify(result.auth));
         }
-    },[navigate])
+      } catch (error) {
+        console.error("Failed to fetch:", error);
+      }
+    }
+  };
 
-    const collectData = async () => {
+  return (
+    <div className="signup">
+      <h1>E-Comm-Deshboard</h1>
+      <h1>Register to access website</h1>
+      <div className="form">
+        <input
+          required
+          value={name}
+          className="input-box"
+          id="name"
+          type="text"
+          placeholder="Enter Name"
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        if (name === "" || email === "" || password === "") {
-            setSignupmsg("fill all inputs")
-        } else {
-            const user = { name, email, password };
-            console.log(name, password, email);
+        <input
+          required
+          value={email}
+          className="input-box"
+          id="email"
+          type="email"
+          placeholder="Enter Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
-            try {
-                const response = await fetch('http://localhost:5000/signup', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(user)
-                });
+        <input
+          required
+          value={password}
+          className="input-box"
+          id="pass"
+          type="password"
+          placeholder="New Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-                const result = await response.json();
-                console.log(result);
-                setSignupmsg('User Sign Up Done')
-
-                if (response) {
-                    navigate('/')
-                    localStorage.setItem("user", JSON.stringify(result))
-                }
-
-            } catch (error) {
-                console.error('Failed to fetch:', error);
-            }
-        }
-    };
-
-
-    return (
-        <div className='signup'>
-            <h1>E-Comm-Deshboard</h1>
-            <h1>Register to access website</h1>
-            <div className='form'>
-                <input
-                    required
-                    value={name}
-                    className='input-box'
-                    id='name'
-                    type="text"
-                    placeholder='Enter Name'
-                    onChange={(e) => setName(e.target.value)}
-                />
-
-                <input
-                    required
-                    value={email}
-                    className='input-box'
-                    id='email'
-                    type="email"
-                    placeholder='Enter Email'
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-
-                <input
-                    required
-                    value={password}
-                    className='input-box'
-                    id='pass'
-                    type="password"
-                    placeholder='New Password'
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-
-                <button onClick={collectData} id='signup-btn'>Sign Up</button>
-                <p>{signupmsg}</p>
-            </div>
-            <div id="login">
-                <p>Already account ?? then</p>
-                <button id='login-btn' ><Link to="/login">Login</Link></button>
-            </div>
-        </div>
-    );
+        <button onClick={collectData} id="signup-btn">
+          Sign Up
+        </button>
+        <p>{signupmsg}</p>
+      </div>
+      <div id="login">
+        <p>Already account ?? then</p>
+        <button id="login-btn">
+          <Link to="/login">Login</Link>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
